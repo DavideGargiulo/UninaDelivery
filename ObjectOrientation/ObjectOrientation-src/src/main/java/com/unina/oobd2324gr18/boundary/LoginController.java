d69c7f98fc96fd0d0bf7287b33a744aacecf57f1
DavideGargiulo <davide.gargiulo002@gmail.com>
2024-07-03 18:49:26 +0200

    fix: Fixed some problems

--- ObjectOrientation/ObjectOrientation-src/src/main/java/com/unina/oobd2324gr18/boundary/LoginController.java
+++ ObjectOrientation/ObjectOrientation-src/src/main/java/com/unina/oobd2324gr18/boundary/LoginController.java
@@ -1,1 +1,26 @@
 package com.unina.oobd2324gr18.boundary;
+
+import com.unina.oobd2324gr18.control.LoginControl;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class LoginController extends BasePageController<LoginControl> {
+
+  @FXML
+  private TextField emailTextField;
+
+  @FXML
+  private PasswordField passwordTextField;
+
+  @FXML
+  private Button loginButton;
+
+  @FXML
+  private TextField plainPasswordTextField;
+}
