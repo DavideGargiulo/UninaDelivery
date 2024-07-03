f71e755f2efb63e7d524c0b4de181e5026037d3b
DavideGargiulo <davide.gargiulo002@gmail.com>
2024-07-03 18:44:48 +0200

    feat: Implemented and finished AccountDAO

--- ObjectOrientation/ObjectOrientation-src/src/main/java/com/unina/oobd2324gr18/DAO/AccountDAO.java
+++ ObjectOrientation/ObjectOrientation-src/src/main/java/com/unina/oobd2324gr18/DAO/AccountDAO.java
@@ -1,5 +1,27 @@
 package com.unina.oobd2324gr18.DAO;
 
-public class AccountDAO {
+import com.unina.oobd2324gr18.DTO.AccountDTO;
+import com.unina.oobd2324gr18.DTO.OperatorDTO;
+import com.unina.oobd2324gr18.DTO.WarehouseDTO;
+import com.unina.oobd2324gr18.DTO.DriverDTO;
 
+import java.util.List;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+
+public interface AccountDAO extends BasicDAO<AccountDTO> {
+
+  AccountDTO findAccountByEmail(String email) throws SQLException;
+
+  AccountDTO findAccountByEmailNPassword(String email, String password) throws SQLException;
+
+  OperatorDTO findOperatorByBusinessemailNPassword(String businessEmail, String password) throws SQLException;
+
+  List<AccountDTO> findAccountNameNSurname(String name, String surname) throws SQLException;
+
+  AccountDTO findMostSpendingAccount(Year year, Month month) throws SQLException;
+
+  List<DriverDTO> findCompatiblesDrivers(WarehouseDTO warehouse, LocalDate date) throws SQLException;
 }
