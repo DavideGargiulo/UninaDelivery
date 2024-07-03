48314b324f4a0980f760d34d2a254508a24cbf8c
DavideGargiulo <davide.gargiulo002@gmail.com>
2024-07-03 12:56:07 +0200

    feat: Finished implementation of OperatorDTO

--- ObjectOrientation/ObjectOrientation-src/src/main/java/com/unina/oobd2324gr18/DTO/OperatorDTO.java
+++ ObjectOrientation/ObjectOrientation-src/src/main/java/com/unina/oobd2324gr18/DTO/OperatorDTO.java
@@ -3,42 +3,66 @@ package com.unina.oobd2324gr18.DTO;
 import java.time.LocalDate;
 
 public class OperatorDTO extends AccountDTO {
+  private String businessEmail;
 
-  // Attributes
-  private String businessMail;
-
-  // Private constructor
-  private OperatorDTO(String accName, String accSurname, String accEmail, LocalDate accBdate, String accPropic,
-                   String accPassword, SavedAddressDTO accAddress, String businessMail) {
-    super(accName, accSurname, accEmail, accBdate, accPropic, accPassword, accAddress);
-    this.businessMail = businessMail;
+  private OperatorDTO(final OperatorBuilder builder) {
+    super(builder.accountName, builder.accountSurname, builder.accountEmail, builder.accountPassword, builder.accountBirthDate, builder.accountAddress);
+    this.businessEmail = builder.businessEmail;
   }
 
-  // Factory method
-  public static OperatorDTO createOperator(String accName, String accSurname, String accEmail, LocalDate accBdate, String accPassword, SavedAddressDTO accAddress, String businessMail) {
-    return new OperatorDTO(accName, accSurname, accEmail, accBdate, accPassword, accAddress, businessMail);
+  public static class OperatorBuilder {
+    private String accountName;
+
+    private String accountSurname;
+
+    private String accountEmail;
+
+    private LocalDate accountBirthDate;
+
+    private String accountPassword;
+
+    private SavedAddressDTO accountAddress;
+
+    private String businessEmail;
+
+    public OperatorBuilder(final String accountName, final String accountSurname, final String accountEmail, final LocalDate accountBirthDate, final String accountPassword, final SavedAddressDTO accountAddress, final String businessEmail) {
+      this.accountName = accountName;
+      this.accountSurname = accountSurname;
+      this.accountEmail = accountEmail;
+      this.accountBirthDate = accountBirthDate;
+      this.accountPassword = accountPassword;
+      this.accountAddress = accountAddress;
+      this.businessEmail = businessEmail;
+    }
+
+    public OperatorDTO build() {
+      return new OperatorDTO(this);
+    }
   }
 
-  // Getters and Setters
-  public String getBusinessMail() {
-    return businessMail;
+  public String getBusinessEmail() {
+    return businessEmail;
   }
 
-  public void setBusinessMail(String businessMail) {
-    this.businessMail = businessMail;
+  public OperatorDTO (final AccountDTO account, final String businessEmail) {
+    super(account.getName(), account.getSurname(), account.getEmail(), account.getPassword(), account.getBirthDate(), account.getAddress());
+    this.businessEmail = businessEmail;
   }
 
-  // Override toString
+  public AccountDTO getAccount() {
+    return new AccountDTO(getName(), getSurname(), getEmail(), getPassword(), getBirthDate(), getAddress());
+  }
+
   @Override
   public String toString() {
-    return "Operator{" +
-            "name='" + getName() + '\'' +
-            ", surname='" + getSurname() + '\'' +
-            ", email='" + getEmail() + '\'' +
-            ", birthdate='" + getBirthDate() + '\'' +
-            ", password='" + getPassword() + '\'' +
-            ", address=" + getAddress().toString().replace("\n", "\n\t") +
-            ", businessMail='" + businessMail + '\'' +
-            '}';
+    return "OperatorDTO{" +
+      "businessEmail='" + getBusinessEmail() + '\'' +
+      ", name='" + getName() + '\'' +
+      ", surname='" + getSurname() + '\'' +
+      ", email='" + getEmail() + '\'' +
+      ", password='" + getPassword() + '\'' +
+      ", birthDate=" + getBirthDate() +
+      ", address=" + getAddress() +
+      '}';
   }
 }
