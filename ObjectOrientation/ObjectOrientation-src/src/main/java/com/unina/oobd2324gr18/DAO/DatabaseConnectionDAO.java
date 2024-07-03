690e3506d11decb4a7c00ef1de6ca381a0922779
DavideGargiulo <davide.gargiulo002@gmail.com>
2024-07-03 18:46:13 +0200

    feat: Implemented and finished Database connection with jdbc

---
+++ ObjectOrientation/ObjectOrientation-src/src/main/java/com/unina/oobd2324gr18/DAO/DatabaseConnectionDAO.java
@@ -0,0 +1,53 @@
+package com.unina.oobd2324gr18.DAO;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
+
+public final class DatabaseConnectionDAO {
+  private static DatabaseConnectionDAO dbconn = null;
+
+  private static Connection conn = null;
+
+  private DatabaseConnectionDAO() {
+    // Costruttore privato
+  }
+
+  static DatabaseConnectionDAO getInstance() {
+    if (dbconn == null) {
+      dbconn = new DatabaseConnectionDAO();
+    }
+    return dbconn;
+  }
+
+  static Connection getConnectionBySchema(final String schema) throws IOException, ClassNotFoundException {
+    String password = null;
+    BufferedReader buffer = null;
+
+    if (Objects.equals(schema, "")) {
+      throw new RuntimeException("Schema name is empty");
+    }
+    try {
+      if (conn == null || conn.isClosed()) {
+        InputStream is = DatabaseConnectionDAO.class.getResourceAsStream("/password.txt");
+        if (is == null) {
+          throw new FileNotFoundException("File password.txt not found");
+        }
+        buffer = new BufferedReader(new InputStreamReader(is));
+        password = buffer.readLine();
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/" + schema;
+        conn = DriverManager.getConnection(url, "postgres", password);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return conn;
+  }
+}
