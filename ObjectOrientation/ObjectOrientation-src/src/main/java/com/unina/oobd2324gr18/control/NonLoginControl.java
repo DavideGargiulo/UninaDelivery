ff5a1b380a98978c9140bf6854d2cbc715c038b9
DavideGargiulo <davide.gargiulo002@gmail.com>
2024-07-13 18:31:25 +0200

    feat: Initialized implementation of NonLoginPage
    - Page about thing that aren't login

---
+++ ObjectOrientation/ObjectOrientation-src/src/main/java/com/unina/oobd2324gr18/control/NonLoginControl.java
@@ -0,0 +1,11 @@
+package com.unina.oobd2324gr18.control;
+
+public class NonLoginControl extends BasicControl {
+  protected static final int WIDTH = 1080;
+
+  protected static final int HEIGHT = 720;
+
+  protected NonLoginControl(final String defaultPageName) {
+    super(defaultPageName);
+  }
+}
