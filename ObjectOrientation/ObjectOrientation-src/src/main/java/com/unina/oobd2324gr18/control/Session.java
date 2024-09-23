c3859530bcc0323535336390d163778c4f431a63
DavideGargiulo <davide.gargiulo002@gmail.com>
2024-09-24 00:19:05 +0200

    fix: fixed minor problem in session

--- ObjectOrientation/ObjectOrientation-src/src/main/java/com/unina/oobd2324gr18/control/Session.java
+++ ObjectOrientation/ObjectOrientation-src/src/main/java/com/unina/oobd2324gr18/control/Session.java
@@ -62,7 +62,7 @@ public final class Session {
    * @return L'ordine selezionato.
    */
   public static OrderDTO getSelectedOrder() {
-    return selectedOrder;
+    return Session.selectedOrder;
   }
 
   /**
